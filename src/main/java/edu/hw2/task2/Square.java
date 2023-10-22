3b40e7af4bde279d78b423f04b69bdc681556f1a
Varyzgin <dima5varyzgin@gmail.com>
2023-10-22 13:55:08 +0300

    fully completed 1, 2 tasks. 4th task doesn't pass stylechecker

---
+++ src/main/java/edu/hw2/task2/Square.java
@@ -0,0 +1,13 @@
+package edu.hw2.task2;
+
+public class Square extends Rectangle {
+    @Override
+    void setWidth(int width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    void setHeight(int height) {
+        super.setHeight(height);
+    }
+}
---
+++ src/main/java/edu/hw2/task3/PopularCommandExecutor.java
@@ -0,0 +1,13 @@
+package edu.hw2.task3;
+
+public final class PopularCommandExecutor {
+
+//    private final ConnectionManager manager;
+//    private final int maxAttempts;
+//
+//    public void updatePackages() {
+//        tryExecute("apt update && apt upgrade -y");
+//    }
+//
+//    void tryExecute(String command) { ... }
+}
